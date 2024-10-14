762f88f6e68cd731a37c29abf70d80aaea7643cd
Nathan Bengio <Nathan Bengio@LAPTOP-PI06HU2U>
2024-10-14 12:18:40 +0200

    Phase 2 : Tâche 1 nettoyer le code

--- src/Controller/FormationsController.php
+++ src/Controller/FormationsController.php
@@ -15,6 +15,7 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class FormationsController extends AbstractController {
 
+    const ADRESSE = "pages/formations.html.twig";
     /**
      * 
      * @var FormationRepository
@@ -36,7 +37,7 @@ class FormationsController extends AbstractController {
     public function index(): Response{
         $formations = $this->formationRepository->findAll();
         $categories = $this->categorieRepository->findAll();
-        return $this->render("pages/formations.html.twig", [
+        return $this->render(self::ADRESSE, [
             'formations' => $formations,
             'categories' => $categories
         ]);
@@ -46,7 +47,7 @@ class FormationsController extends AbstractController {
     public function sort($champ, $ordre, $table=""): Response{
         $formations = $this->formationRepository->findAllOrderBy($champ, $ordre, $table);
         $categories = $this->categorieRepository->findAll();
-        return $this->render("pages/formations.html.twig", [
+        return $this->render(self::ADRESSE, [
             'formations' => $formations,
             'categories' => $categories
         ]);
@@ -57,7 +58,7 @@ class FormationsController extends AbstractController {
         $valeur = $request->get("recherche");
         $formations = $this->formationRepository->findByContainValue($champ, $valeur, $table);
         $categories = $this->categorieRepository->findAll();
-        return $this->render("pages/formations.html.twig", [
+        return $this->render(self::ADRESSE, [
             'formations' => $formations,
             'categories' => $categories,
             'valeur' => $valeur,
